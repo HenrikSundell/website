ecd69581c7c5af03a3d5495a1c6e2817f6d33a34
Henrik Sundell <henrik.sundell@gmail.com>
2020-11-25 18:01:03 +0200

    Change link style in AboutSection text
    Change link color to black to mach the text and red on hower
    to make it clear that they are links.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -26,17 +26,23 @@ function AboutSection({
         I'have experience in web development from my summer job 2020. I worked
         at the computer science department at aalto developing educational tech.
         Mostly django backend and REST api related tasks. You can find some of
-        the work on.
-        <a href="https://github.com/apluslms/a-plus"> GitHub </a>since it is
-        open source.
+        the work on.{" "}
+        <a className="about-link" href="https://github.com/apluslms/a-plus">
+          GitHub
+        </a>{" "}
+        since it is open source.
       </p>
       <h1 className="subtitle">Education</h1>
       <p>
-        I' currently planning to finnish my bachelor in computer science at
-        <a href="https://aalto.fi/en"> Aalto University. </a> On top of that I'm
-        studying finance at
-        <a href="https://www.hanken.fi/en"> Hanken School of Economics </a> as a
-        minor for my bachelor.
+        I' currently planning to finnish my bachelor in computer science at{" "}
+        <a className="about-link" href="https://aalto.fi/en">
+          Aalto University
+        </a>
+        . On top of that I'm studying finance at{" "}
+        <a className="about-link" href="https://www.hanken.fi/en">
+          Hanken School of Economics
+        </a>{" "}
+        as a minor for my bachelor.
       </p>
     </section>
   );
